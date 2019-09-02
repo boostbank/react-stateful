505efc36f98904ecc3634ecc41c820c9b560169e
dbteku <dbteku@hotmail.com>
2019-09-02 16:32:04 -0600

    I'm struggling to make a decision on how to pass stuff.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-import PartitionProvider from "./components/PartitionProvider";
 // import { createStore, modify } from "@boostbank/stateful";
 
 class App extends Component {
   render() {
-    // setTimeout(() => {
-    //   modify(state => {
-    //     state.test = "Hello World";
-    //     return state;
-    //   });
-    // }, 5000);
     return (
-      <PartitionProvider>
         <div className="App">Test</div>
-      </PartitionProvider>
     );
   }
 }
